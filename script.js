dd13a482080e6902c2a6bc920c71cfc3042435db
Renz <140231442+lawrnzzz@users.noreply.github.com.>
2024-11-10 00:52:45 +0800

    feat: form data from the modal now renders a new book each submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function toggleAddMangaModal() {
   const close = document.querySelector(".modal-close");
   const dialog = document.querySelector("#add-manga-modal");
   const cancel = document.querySelector(".modal-footer > .cancel-btn");
-
+  const form = document.querySelector("#modal-form");
   addManga.addEventListener("click", () => {
     dialog.showModal();
   });
@@ -15,28 +15,10 @@ function toggleAddMangaModal() {
   cancel.addEventListener("click", () => {
     dialog.close();
   });
-}
 
-const myLibrary = [];
-
-function Book(title, author, chapters, readStatus) {
-  this.title = title;
-  this.author = author;
-  this.chapters = chapters;
-  this.readStatus = readStatus;
-  this.info = () => {
-    console.log(
-      `Title: ${this.title}, Author: ${this.author}, No. of Chapters: ${this.chapters}, Read status: ${this.readStatus}`
-    );
-  };
-}
-
-function createBook(title, author, chapters, readStatus) {
-  return new Book(title, author, chapters, readStatus);
-}
-
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+  dialog.addEventListener("close", () => {
+    form.reset();
+  });
 }
 
 function createMangaCover() {
@@ -127,7 +109,51 @@ function displayBook(book) {
   cardContainer.appendChild(cardItem);
 }
 
-const book = createBook("One Piece", "Eiichiro Oda", 1120, true);
-displayBook(book);
+const myLibrary = [];
+
+function Book(title, author, chapters, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.chapters = chapters;
+  this.readStatus = readStatus;
+  this.info = () => {
+    console.log(
+      `Title: ${this.title}, Author: ${this.author}, No. of Chapters: ${this.chapters}, Read status: ${this.readStatus}`
+    );
+  };
+}
+
+function createBook(title, author, chapters, readStatus) {
+  return new Book(title, author, chapters, readStatus);
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function submitFormData() {
+  const addMangaModal = document.querySelector("#add-manga-modal");
+  const modalForm = document.querySelector("#modal-form");
+
+  modalForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const title = document.querySelector('input[name="title"]').value;
+    const author = document.querySelector('input[name="author"]').value;
+    const chapters = document.querySelector('input[name="chapters"]').value;
+    const read = document.querySelector('input[name="read"]').checked;
+
+    const book = createBook(title, author, chapters, read);
+    updatePreview(book);
+
+    addMangaModal.close();
+  });
+}
+
+function updatePreview(book) {
+  addBookToLibrary(book);
+  displayBook(book);
+}
 
 toggleAddMangaModal();
+submitFormData();
