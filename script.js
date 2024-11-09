e7398f3f85433d05597ca23a1ba741857bc5b7d6
Renz <140231442+lawrnzzz@users.noreply.github.com.>
2024-11-09 20:54:28 +0800

    feat: modal dialog now closes when cancel button is clicked

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const addManga = document.querySelector("#add-manga");
-const close = document.querySelector(".modal-close");
-const dialog = document.querySelector("#add-manga-modal");
+function toggleAddMangaModal() {
+  const addManga = document.querySelector("#add-manga");
+  const close = document.querySelector(".modal-close");
+  const dialog = document.querySelector("#add-manga-modal");
+  const cancel = document.querySelector(".modal-footer > .cancel-btn");
 
-addManga.addEventListener("click", () => {
-  dialog.showModal();
-});
+  addManga.addEventListener("click", () => {
+    dialog.showModal();
+  });
 
-close.addEventListener("click", () => {
-  dialog.close();
-});
+  close.addEventListener("click", () => {
+    dialog.close();
+  });
+
+  cancel.addEventListener("click", () => {
+    dialog.close();
+  });
+}
+
+toggleAddMangaModal();
